45131349857244873f8354a4a4ff87fa79a35e52
DimaProxor <proxortv@gmail.com>
2014-09-02 21:01:45 +0700

    New [1]

--- Game_Data/Scripts/CenterGame.cs
+++ Game_Data/Scripts/CenterGame.cs
@@ -6,6 +6,7 @@ public class CenterGame : MonoBehaviour {
 	public bool CanSpawn;
 	public GameObject[] Objects;
 	public Texture2D TextureOfControl;
+	public GUITexture FirstTexture, SecondTexture;
 	// Use this for initialization
 	void Start () {
 	
@@ -24,5 +25,6 @@ public class CenterGame : MonoBehaviour {
 	{
 		GUI.DrawTexture(new Rect(Screen.width/13, Screen.height/2, 100, 100), TextureOfControl);
 		GUI.DrawTexture(new Rect(Screen.width - Screen.width/13, Screen.height/2, -100, 100), TextureOfControl);
+
 	}
 }
