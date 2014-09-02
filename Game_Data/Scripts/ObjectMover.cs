a1de848b58a6a2144b995b8ec829c2be1c8d1f56
DimaProxor <proxortv@gmail.com>
2014-09-02 19:29:04 +0700

    Начальные скрипты
    Пока что не доделанные скрипты, но скачать всё же стоит...

---
+++ Game_Data/Scripts/ObjectMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
